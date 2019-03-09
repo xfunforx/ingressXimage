ba4175628fb26cd150ffedd8d36a3ff645e1b16e
Cypher01 <cypher@cypher.com>
2019-03-09 14:06:15 +0100

    IngressXimage v1.2, fixed preferences, added support for Scanner [REDACTED], updated build files, removed IntelliJ and unused files

--- app/src/main/java/com/xingress/ximage/MainActivity.java
+++ app/src/main/java/com/xingress/ximage/MainActivity.java
@@ -1,57 +1,149 @@
 package com.xingress.ximage;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
-    private ToggleButton toggleButton;
-    private String xstatus;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final SharedPreferences sharedPreferences = getSharedPreferences("ximage", 1);
-        final SharedPreferences.Editor editor = sharedPreferences.edit();
-        final boolean ximage = sharedPreferences.getBoolean("ximage", false);
-        toggleButton = (ToggleButton) findViewById(R.id.ximage);
-        toggleButton.setChecked(ximage);
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    xstatus = getString(R.string.close);
-                } else {
-                    xstatus = getString(R.string.open);
-                }
-                Snackbar.make(buttonView, getString(R.string.message) + xstatus, Snackbar.LENGTH_LONG)
-                        .show();
-                editor.putBoolean("ximage", isChecked);
-                editor.commit();
-            }
-        });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent emailIntent = new Intent(Intent.ACTION_SEND);
-                String[] recipients = new String[]{"xfunforx@gmail.com"};
-                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, recipients);
-                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "hi, xfunforx this is about ximage");
-                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "i need for some help.");
-                emailIntent.setType("text/plain");
-                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            }
-        });
-    }
+public class MainActivity extends AppCompatActivity {
+	public static final String TAG = "IngressXimage";
+
+	public static final String INGRESSPACKAGENAME = "com.nianticproject.ingress";
+	public static final String SCANNERREDACTEDPACKAGENAME = "com.nianticlabs.ingress.prime.qa";
+	public static final String XIMAGE = "ximage";
+
+	private String xstatus;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		final SharedPreferences sharedPreferences = getSharedPreferences(XIMAGE, Context.MODE_PRIVATE);
+		final SharedPreferences.Editor editor = sharedPreferences.edit();
+		final Resources res = getResources();
+
+		final boolean ximage = sharedPreferences.getBoolean(XIMAGE, false);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+		String buildDate = dateFormat.format(new java.util.Date(BuildConfig.TIMESTAMP));
+
+		String text = res.getString(R.string.app_name) + " " + String.format(res.getString(R.string.credits_version), BuildConfig.VERSION_NAME, buildDate);
+
+		TextView tv_credits = findViewById(R.id.tv_credits);
+		tv_credits.setText(text);
+
+		ToggleButton toggleButton = findViewById(R.id.ximage);
+		toggleButton.setChecked(ximage);
+		toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					xstatus = getString(R.string.close);
+				} else {
+					xstatus = getString(R.string.open);
+				}
+
+				Snackbar.make(buttonView, getString(R.string.message) + xstatus, Snackbar.LENGTH_LONG).show();
+
+				editor.putBoolean(XIMAGE, isChecked).commit();
+			}
+		});
+
+		FloatingActionButton fab = findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent emailIntent = new Intent(Intent.ACTION_SEND);
+				String[] recipients = new String[]{"xfunforx@gmail.com"};
+				emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, recipients);
+				emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "hi, xfunforx this is about ximage");
+				emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "i need for some help.");
+				emailIntent.setType("text/plain");
+				startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+			}
+		});
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		fixPermissions();
+	}
+
+	private void fixPermissions() {
+		boolean dataFolderExists = false;
+		boolean prefFolderExists = false;
+		boolean prefFileExists = false;
+
+		boolean dataFolderReadable = false;
+		boolean dataFolderExecutable = false;
+		boolean prefFolderReadable = false;
+		boolean prefFolderExecutable = false;
+		boolean prefFileReadable = false;
+
+		File dataFolder = new File(this.getApplicationInfo().dataDir);
+
+		if (dataFolder.exists()) { // this should be true, as soon as the app has been started
+			dataFolderExists = true;
+			dataFolderReadable = dataFolder.setReadable(true, false);
+			dataFolderExecutable = dataFolder.setExecutable(true, false);
+
+			File sharedPrefsFolder = new File(this.getApplicationInfo().dataDir + "/shared_prefs");
+			//Log.d(TAG, "sharedPrefsFolder: " + sharedPrefsFolder.toString());
+
+			if (sharedPrefsFolder.exists()) {
+				prefFolderExists = true;
+				prefFolderReadable = sharedPrefsFolder.setReadable(true, false);
+				prefFolderExecutable = sharedPrefsFolder.setExecutable(true, false);
+
+				File prefFile = new File(sharedPrefsFolder.getAbsolutePath() + "/" + XIMAGE + ".xml");
+
+				if (prefFile.exists()) {
+					prefFileExists = true;
+					prefFileReadable = prefFile.setReadable(true, false);
+				} else {
+					Log.e(TAG, "preferences file doesn't exist");
+				}
+			} else {
+				Log.e(TAG, "preferences folder doesn't exist");
+			}
+		} else {
+			Log.e(TAG, "data folder doesn't exist");
+		}
+
+		if (dataFolderExists && !dataFolderReadable) {
+			Log.e(TAG, "data folder set readable FAILED");
+		}
+
+		if (dataFolderExists && !dataFolderExecutable) {
+			Log.e(TAG, "data folder set executable FAILED");
+		}
+
+		if (prefFolderExists && !prefFolderReadable) {
+			Log.e(TAG, "preferences folder set readable FAILED");
+		}
+
+		if (prefFolderExists && !prefFolderExecutable) {
+			Log.e(TAG, "preferences folder set executable FAILED");
+		}
+
+		if (prefFileExists && !prefFileReadable) {
+			Log.e(TAG, "preferences file set readable FAILED");
+		}
+	}
 }
--- app/src/main/java/com/xingress/ximage/Ximage.java
+++ app/src/main/java/com/xingress/ximage/Ximage.java
@@ -3,30 +3,38 @@ package com.xingress.ximage;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static com.xingress.ximage.MainActivity.*;
+import static de.robv.android.xposed.XposedBridge.log;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class Ximage implements IXposedHookLoadPackage {
-    @Override
-    public void handleLoadPackage(LoadPackageParam loadPackageParam) throws Throwable {
-        if (!loadPackageParam.packageName.equals("com.nianticproject.ingress")) {
-            return;
-        }
-        final XSharedPreferences xSharedPreferences = new XSharedPreferences("com.xingress.ximage", "ximage");
-        XposedBridge.log("found the ingress started: " + loadPackageParam.packageName);
-        findAndHookMethod("com.nianticproject.ingress.gameentity.components.portal.SimplePhotoStreamInfo", loadPackageParam.classLoader, "getCoverPhoto", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                xSharedPreferences.reload();
-                boolean toggle = xSharedPreferences.getBoolean("ximage", false);
-                XposedBridge.log("toggle:" + String.valueOf(toggle));
-                if (toggle) {
-                    param.setResult(null);
-                }
-            }
-        });
-    }
-}
+	public static final String className = "com.nianticproject.ingress.gameentity.components.portal.SimplePhotoStreamInfo";
+	public static final String methodName = "getCoverPhoto";
 
+	@Override
+	public void handleLoadPackage(LoadPackageParam loadPackageParam) {
+		if (!loadPackageParam.packageName.equals(INGRESSPACKAGENAME) && !loadPackageParam.packageName.equals(SCANNERREDACTEDPACKAGENAME)) {
+			return;
+		}
+
+		log("Found the Ingress started: " + loadPackageParam.packageName);
+
+		final XSharedPreferences xSharedPreferences = new XSharedPreferences(Ximage.class.getPackage().getName(), XIMAGE);
+
+		findAndHookMethod(className, loadPackageParam.classLoader, methodName, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) {
+				xSharedPreferences.reload();
+				boolean toggle = xSharedPreferences.getBoolean(XIMAGE, false);
+
+				log("toggle: " + String.valueOf(toggle));
+
+				if (toggle) {
+					param.setResult(null);
+				}
+			}
+		});
+	}
+}
